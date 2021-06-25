462325bcc08902248b6443dc1d15f6ba7c17c8ed
ck <ck@gmail.com>
2021-06-25 17:33:52 +0800

    master继续开发...。。。。

--- src/main/java/cn/com/tskjxy/git/GitTest.java
+++ src/main/java/cn/com/tskjxy/git/GitTest.java
@@ -7,5 +7,13 @@ public class GitTest {
         //hot-fix修复bug
         System.out.println("修复bug，master分支继续开发...");
         System.out.println("修复bug之后了修改");
+        System.out.println("修复bug之后了修改");
+        System.out.println("修复bug之后了修改");
+        System.out.println("修复bug之后了修改");
+        System.out.println("修复bug之后了修改");
+        System.out.println("修复bug之后了修改");
+
+
+
     }
 }
